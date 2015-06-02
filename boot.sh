85c98bd5111793954750689fd87757a8f0175b85
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2015-06-01 23:14:00 -0700

    initial commit

---
+++ boot.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+sudo apt-get update 2> /dev/null
+
+#sudo apt-get install -y make 2> /dev/null
+
+#sudo apt-get install -y nodejs 2> /dev/null
+
+#sudo apt-get install -y nodejs-dev 2/> /dev/null
+
+#sudo apt-get install -y npm 2> /dev/null
+
