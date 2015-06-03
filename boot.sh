f6c33e59c920f1ed6996a3b7d4d8b0001202d624
Souriya Khaosanga <souriya@janrain.com>
2015-06-02 20:14:39 -0700

    fix boot.sh

--- boot.sh
+++ boot.sh
@@ -3,6 +3,8 @@
 
 sudo apt-get update 2> /dev/null
 
+docker pull tutum/mongodb 2> /dev/null
+
 #sudo apt-get install -y make 2> /dev/null
 
 #sudo apt-get install -y nodejs 2> /dev/null
@@ -12,5 +14,7 @@ sudo apt-get update 2> /dev/null
 sudo apt-get install -y npm 2> /dev/null
 sudo apt-get install -y nodejs-legacy 2> /dev/null
 sudo npm install npm -g 2> /dev/null
+
+docker run -d -p 27017:27017 -p 28017:28017 -e AUTH=no tutum/mongodb 2> /dev/null
 
 sudo npm -g install sails 2> /dev/null
